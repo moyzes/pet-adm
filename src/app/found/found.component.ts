509004f9a7b7b6798fa17045f0fdc6a0b4fd215f
Braz Moyzes <moyzes.braz@volvo.com>
2019-07-16 21:34:00 -0300

    Atualizações stepper, em andamento.

--- src/app/found/found.component.ts
+++ src/app/found/found.component.ts
@@ -1,37 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-found',
-  templateUrl: './found.component.html',
-  styleUrls: ['./found.component.css']
+	selector: 'app-found',
+	templateUrl: './found.component.html',
+	styleUrls: ['./found.component.css']
 })
+
 export class FoundComponent implements OnInit {
 
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-testhtml = `<form [formGroup]="firstFormGroup">
-    <ng-template matStepLabel>Fill out your name</ng-template>
-    <mat-form-field>
-      <input matInput placeholder="Last name, First name" formControlName="firstCtrl" required>
-    </mat-form-field>
-    <div>
-      <button mat-button matStepperNext>Next</button>
-    </div>
-</form>`;
+	formGroup: FormGroup;
+	constructor(private _formBuilder: FormBuilder) { }
 
-
-  constructor(private _formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-
-  }
-
+	ngOnInit() {
+		this.formGroup = this._formBuilder.group({
+			formArray: this._formBuilder.array([
+				this._formBuilder.group({}),
+				this._formBuilder.group({})
+			])
+		});
+	}
 }
